c8f977acee7ac7534a7a4f01b53b9c1f62c9d037
splwebdev <shane_lynch@hotmail.com>
2023-02-17 09:00:26 +0000

    Added objects to questions array variable

--- index.js
+++ index.js
@@ -5,7 +5,81 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What is the description of your project?",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What are the installation instructions?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "What are the contribution guidelines",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "What are the test instructions?",
+    name: "test",
+  },
+  {
+    type: "list",
+    message: "What license would you like to use?",
+    name: "license",
+    choice: [
+      "Apache License 2.0",
+      "Academic Free License v3.0",
+      "Artistic license 2.0",
+      "Boost Software License 1.0",
+      'BSD 2-clause "Simplified" license',
+      'BSD 3-clause "New" or "Revised" license',
+      "BSD 3-clause Clear license",
+      "Creative Commons license family",
+      "Creative Commons Zero v1.0 Universal",
+      "Creative Commons Attribution 4.0",
+      "Creative Commons Attribution Share Alike 4.0",
+      "Do What The F*ck You Want To Public License",
+      "Educational Community License v2.0",
+      "Eclipse Public License 1.0",
+      "Eclipse Public License 2.0",
+      "European Union Public License 1.1",
+      "GNU Affero General Public License v3.0",
+      "GNU General Public License family",
+      "GNU General Public License v2.0",
+      "GNU General Public License v3.0",
+      "GNU Lesser General Public License v2.1",
+      "GNU Lesser General Public License v3.0",
+      "ISC",
+      "LaTeX Project Public License v1.3c",
+      "Microsoft Public License",
+      "MIT",
+      "Mozilla Public License 2.0",
+      "Open Software License 3.0",
+      "PostgreSQL License",
+      "SIL Open Font License 1.1",
+      "University of Illinois/NCSA Open Source License",
+      "The Unlicense",
+      "zLib License",
+    ],
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
 ];
 
 // function to write README file
